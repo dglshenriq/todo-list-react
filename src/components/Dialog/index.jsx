3e633642f3c0a2295aebdfec0aa0ecc71eb8c8b4
dglshenriq <dglsramone@gmail.com>
2026-08-24 17:42:39 -0300

    Otimiza o Dialog utilizando useRef

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,27 +1,27 @@
-import './dialog.style.css'
+import React, { useRef } from "react";
+import "./dialog.style.css";
 
 export function Dialog() {
-  const dialog = document.querySelector("dialog");
-  const showButton = document.querySelector("dialog + button");
-  const closeButton = document.querySelector("dialog button");
+  const dialogRef = useRef(null)
 
-  // "Show the dialog" button opens the dialog modally
-  showButton.addEventListener("click", () => {
-    dialog.showModal();
-  });
 
-  // "Close" button closes the dialog
-  closeButton.addEventListener("click", () => {
-    dialog.close();
-  });
+  const openDialog = () => {
+    dialogRef.current.showModal();
+  };
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+  };
 
   return (
     <>
-      <dialog>
-        <button autofocus>Close</button>
+      <dialog ref={dialogRef}>
+        <button autoFocus onClick={closeDialog}>
+          Close
+        </button>
         <p>This modal dialog has a groovy backdrop!</p>
       </dialog>
-      <button>Show the dialog</button>
+      <button onClick={openDialog}>Show the dialog</button>
     </>
   );
 }
